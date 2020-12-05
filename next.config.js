886595d1d34c9789560e4cc4280ae3ff65ec861f
Syedzainjeelani <sayedzainjeelani@gmail.com>
2020-12-05 23:07:58 +0500

    Some Config changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,5 @@ module.exports = {
     env: {
         // MONGO_URI: "mongodb://localhost:27017/tododb"
         MONGO_URI: "mongodb+srv://Todo-db:5vGK4iM3DiWyYyqb@cluster0.fykto.mongodb.net/tododb?retryWrites=true&w=majority"
-        // MONGO_URI: "mongodb+srv://dbTodo:5vGK4iM3DiWyYyqb@cluster0.fykto.mongodb.net/tododb?retryWrites=true&w=majority"
     }
 }
