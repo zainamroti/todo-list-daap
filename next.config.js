787d1e0e8a299969f0ebf624e8ae0919eebcc16b
Syedzainjeelani <sayedzainjeelani@gmail.com>
2020-12-06 00:32:26 +0500

    Remove next.config.js from local git

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    env: {
-        // MONGO_URI: "mongodb://localhost:27017/tododb"
-        MONGO_URI: "mongodb+srv://Todo-db:5vGK4iM3DiWyYyqb@cluster0.fykto.mongodb.net/tododb?retryWrites=true&w=majority"
-    }
-}
