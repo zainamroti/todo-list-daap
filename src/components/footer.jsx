0f1aa133f95d2c075624581d058954391fa2c768
Syedzainjeelani <sayedzainjeelani@gmail.com>
2021-06-15 00:43:51 +0500

    Footer name removed, for Viva project.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,13 +4,14 @@ import React from 'react'
 export default function Footer() {
   return <React.Fragment>
     <footer className={styles.footer}>
-      <a
+      {/* <a
         href="https://syedzainjeelani.github.io/cv"
         target="_blank"
         rel="noopener noreferrer"
-      >
-        Created by{' Syed Zain Jeelani'}
-      </a>
+      > */}
+      {/* Created with {' Syed Zain Jeelani'} */}
+      {/* All Rights Reserved. */}
+      {/* </a> */}
     </footer>
   </React.Fragment>;
 };
